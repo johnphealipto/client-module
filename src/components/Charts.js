e6e46602d7f45fac58043b0dcf872ad61a37d28a
johnphealipto <johnadibe450@gmail.com>
2021-12-16 17:21:35 +0100

    Chart

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -21,25 +21,43 @@ ChartJS.register(
 
 const Charts = () => {
 
-  const barConfig = {
+  const barData = {
     labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
     datasets: [
       {
         label: "Expected Repayment",
         backgroundColor: "#CFEBFC",
-        data: [3,7,4]
+        data: [85,25,60,55,33,25,41,39,45,85,85,85],
+        borderRadius: 75,
+        borderSkipped: false,
+        barPercentage: 0.5,
       },
       {
         label: "Actual Repayment",
         backgroundColor: "#109CF1",
-        data: [4,3,5]
+        data: [15,59,80,55,29,45,25,63,20,15,15,15],
+        borderRadius: 75,
+        borderSkipped: false,
+        barPercentage: 0.5,
+        
       }
     ]
   }
 
   return (
     <div className="dashboard-charts">
-      <Bar data={barConfig} /> 
+      <div className="bar-chart">
+        <div className="bar-chart_header">
+          <p>Expected Repayment vs Actual Repayment</p>
+        </div>
+        <Bar data={barData} /> 
+        <div className="bar-chart_footer">
+          <p>Expected Repayment - Actual Repayment</p>
+        </div>
+      </div>
+      <div className="doughnut-chart">
+        xfgxfhgf
+      </div>
     </div>
   )
 }
